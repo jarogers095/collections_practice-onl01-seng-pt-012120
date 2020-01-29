69bf652f2aef48e8f7c4c6c250c505db88460fb5
James Rogers <jarogers095@gmail.com>
2020-01-29 02:13:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,43 @@
-def sort_array_asc()
+def sort_array_asc(array)
+  return array.sort
+end
+
+
+def sort_array_desc(array)
   
 end
 
 
-def ()
+def sort_array_char_count(array)
+  
+end
+
+
+def swap_elements(array)
+  
+end
+
+
+def reverse_array(array)
+  
+end
+
+
+def kesha_maker(array)
+  
+end
+
+
+def find_a(array)
+  
+end
+
+
+def sum_array(array)
+  
+end
+
+
+def add_s(array)
   
 end
